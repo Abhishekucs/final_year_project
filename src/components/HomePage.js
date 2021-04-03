0b46481cbc5bb9ae35c3e8c7f2d617dd31fa3f7a
Abhishekucs <40858773+Abhishekucs@users.noreply.github.com>
2021-04-03 23:33:50 +0530

    react app

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import ImgSlider from "./ImgSlider";
+
+function HomePage(props) {
+  return (
+    <div>
+      <Header />
+      <ImgSlider />
+    </div>
+  );
+}
+
+export default HomePage;
