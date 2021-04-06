7f136f4990756938d670503945aa968bdf55c2f9
Abhishekucs <40858773+Abhishekucs@users.noreply.github.com>
2021-04-06 19:37:49 +0530

    server added

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("Welcome to Final Year Project");
-
-function greet() {
-  alert("Hello Awesome :)");
-}
-
-function greetBye() {
-  alert("Welcome to the Family XD");
-}
--- onlinevoting/src/components/HomePage.js
+++ onlinevoting/src/components/HomePage.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Button from "./Button";
 import Header from "./Header";
 import ImgSlider from "./ImgSlider";
 
 function HomePage(props) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div>
       <Header />
       <ImgSlider />
+      <Button />
+      <p>{!data ? "Loading..." : data}</p>
+      <div className="footer">
+        <div className="logo_footer">
+          <span>LOGO</span>
+        </div>
+        <div className="content_footer">
+          <div className="content">
+            <span>Terms of use</span>
+          </div>
+          <div className="content">
+            <span>Legal Notice</span>
+          </div>
+          <div className="content">
+            <span>Privacy Issue</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
