3b53625e44e4681aa92c158ed3866681ab633c76
Sinha199 <anshu.resource@gmail.com>
2021-04-07 16:49:40 +0530

    slider dependencies

--- onlinevoting/src/components/ImgSlider.js
+++ onlinevoting/src/components/ImgSlider.js
@@ -1,14 +1,53 @@
-import React from "react";
-import One from "../assests/images/img1.jpg";
-import Two from "../assests/images/img2.jpg";
-import Three from "../assests/images/img3.jpg";
+import "../styles/imgSlider.css";
+import { useState } from "react";
+import Slider from "react-slick";
+import image1 from "../assests/images/img1.jpg";
+import image2 from "../assests/images/img2.jpg";
+import image3 from "../assests/images/img3.jpg";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
-function ImgSlider(props) {
-  return (
-    <div className="imgContainer">
-      <div className="Images">
-        <img src={One} alt="Three" />
+const images = [image1 , image2,image3];
+
+function ImgSlider() {
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight />
       </div>
+    );
+  };
+
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft />
+      </div>
+    );
+  };
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next),
+  };
+
+  return (
+    <div className="ImgSlider">
+      <Slider {...settings}>
+        {images.map((img, idx) => (
+          <div className={idx === imageIndex ? "slide activeSlide" : "slide"}>
+            <img src={img} alt={img} />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
